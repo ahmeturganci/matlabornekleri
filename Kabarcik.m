73aace9d8a88ae021928e3cdf7cd622c1e397acb
ahmeturganci <aahmet.272@gmail.com>
2017-04-12 18:34:04 +0300

    Kabarcik

---
+++ Kabarcik.m
@@ -0,0 +1,11 @@
+function s=Kabarcik(dizi)
+boyut=length(dizi)
+for i=1:1:boyut-1
+    for j=1:1:boyut-i
+        if(dizi(j)>dizi(j+1))
+            temp=dizi(j)
+            dizi(j)=dizi(j+1)
+            dizi(j+1)=temp
+        end
+    end
+end
